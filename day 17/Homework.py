62dcdcbdd08bb869b77cc465c3626094e9f9461a
Datomgela <138392124+Datomgela@users.noreply.github.com>
2023-09-05 20:09:22 +0400

    Delete day 17 directory

--- day 17/Homework.py
+++
@@ -1,4 +0,0 @@
-import random 
-crew_leaders = ['kruashvili', 'amiranashvili', 'tyeshelashvili', 'janezashvili', 'molodini', 'kereselidze', 'kurtanidze']
-for i in range(1,4):
-    print('winner N',i,'is',random.choice(crew_leaders))
