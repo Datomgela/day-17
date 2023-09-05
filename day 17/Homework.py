c28255bb59f018069e42277a51989b9070b006f4
Datomgela <138392124+Datomgela@users.noreply.github.com>
2023-09-05 18:37:19 +0400

    Add files via upload

---
+++ day 17/Homework.py
@@ -0,0 +1,4 @@
+import random 
+crew_leaders = ['kruashvili', 'amiranashvili', 'tyeshelashvili', 'janezashvili', 'molodini', 'kereselidze', 'kurtanidze']
+for i in range(1,4):
+    print('winner N',i,'is',random.choice(crew_leaders))
